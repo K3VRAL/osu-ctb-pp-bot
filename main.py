85c9de45ff5cbc81a228adc6e2051625d0aa4200
K 3 V R A L <kevinbestkiller@gmail.com>
2023-07-20 19:16:26 +0200

    Some minor changes made to test formatting.

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ def main():
         if msg[0] == "!":
             cmd = msg[1:].split(" ")[0]
             msg = msg[1:].split(" ")[1:]
-            if (cmd == "p" or cmd == "profile") and len(msg) == 1:
+
+            if (cmd == "p" or cmd == "profile") and len(msg) >= 1:
                 shell = ["dotnet", "run", "--", "profile", msg[0], config["ID"], config["SECRET"], "-r", "2" , "-j"]
                 print(" ".join(shell))
                 process = subprocess.Popen(shell, cwd=config["PC_PATH"], stdout=subprocess.PIPE)
@@ -35,28 +36,30 @@ def main():
                     except:
                         await thread.send(line.decode("utf-8"))
                         continue
-                
+                if type(objson) != dict:
+                    return
+
                 for key in objson.keys():
                     if key == "Scores":
                         continue
                     await thread.send("{}: {}".format(key, objson[key]))
                 
-                embeds = []
                 for score in objson["Scores"]:
-                    embed = discord.Embed(description="Combo: {}".format())
+                    embed = discord.Embed(description="Combo: {}".format(score["Combo"]))
                     # {'BeatmapId': 1181952, 'BeatmapName': 'Halozy - Snow Changes to a Beat Again (BoberOfDarkness) [Blizzard]', 'Combo': 339, 'Accuracy': 99.75369458128078, 'MissCount': 0.0, 'Mods': [], 'LivePp': 194.243, 'LocalPp': 194.2392055189923, 'PositionChange': 0}
                     embed.set_author(name=score["BeatmapName"], url="https://osu.ppy.sh/b/{}".format(score["BeatmapId"]))
-                    embeds.append(embed)
-                await thread.send(embeds=embeds)
+                    await thread.send(embed=embed)
 
-            elif (cmd == "l" or cmd == "list"):
-                shell = ["git", "branch", "-q"]
-                print(" ".join(shell))
+            elif cmd == "l" or cmd == "list":
+                shell = ["git", "for-each-ref", "--format=%(refname:short)", "refs/heads/"]
                 process = subprocess.Popen(shell, cwd=config["OSU_PATH"], stdout=subprocess.PIPE)
                 thread = await item.channel.create_thread(name="git branch", type=discord.ChannelType.public_thread)
+                text = ""
                 for line in iter(process.stdout.readline, b''):
-                    await thread.send(line.decode("utf-8"))
-            elif (cmd == "h" or cmd == "help"):
+                    text += "{}, ".format(line.decode("utf-8").strip())
+                await thread.send(text[:len(text) - 2])
+
+            elif cmd == "h" or cmd == "help":
                 await item.channel.send("Hello, World!")
 
     client.run(config["TOKEN"])
